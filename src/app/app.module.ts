45466c7341dda6b404f5f3f4ef115f283aef7c90
Филип <filyamintotheface@gmail.com>
2020-08-14 15:47:07 +0300

    added front added import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { InputFieldComponent } from './Components/input-field/input-field.component';
+import { MainTodoHelloComponent } from './Components/main-todo-hello/main-todo-hello.component';
+import { TodoCreatedComponent } from './Components/todo-created/todo-created.component';
+import {FormsModule} from '@angular/forms';
+import { ReactiveFormsModule} from '@angular/forms';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputFieldComponent,
+    MainTodoHelloComponent,
+    TodoCreatedComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
